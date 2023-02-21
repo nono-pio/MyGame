6204512a4d4b91f7cb9aec8a21b96a1448191ab8
nono-pio <112903301+nono-pio@users.noreply.github.com>
2023-02-21 13:54:19 +0100

    Upload Project

---
+++ Assets/BirdScript.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdScript : MonoBehaviour
+{
+
+    public Rigidbody2D Rigidbody;
+    public float flapStrength;
+    public LogicScript logic;
+    public bool BirdIsAlive = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && BirdIsAlive)
+        {
+            Rigidbody.velocity = Vector2.up * flapStrength;
+        }
+    }
+    
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        logic.GameOver();
+        BirdIsAlive = false;
+    }
+}
---
+++ Assets/LogicScript.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LogicScript : MonoBehaviour
+{
+    public int playerScore;
+    public Text scoreText;
+    public GameObject GameOverScreen;
+
+    [ContextMenu("Increase Score")]
+    public void addScore(int ScoreToAdd)
+    {
+        playerScore += ScoreToAdd;
+        scoreText.text = playerScore.ToString();
+    }
+
+    public void restartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void GameOver()
+    {
+        GameOverScreen.SetActive(true);
+    }
+}
